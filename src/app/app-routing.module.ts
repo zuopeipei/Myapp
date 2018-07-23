7cbd7a94f09714e5395b6f4fed0092acc0663503
zuopeiyuan <peiyuan_zuo@baofu.com>
2018-07-23 15:41:52 +0800

    添加和忽略文件

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {RouterModule,Routes} from '@angular/router'
-import { CommandComponent }from './command/command.component';
+import {RouterModule , Routes} from '@angular/router';
+import { CommandComponent } from './command/command.component';
+import {HomeComponent} from './home/home.component';
 
 
-const routes: Routes=[
-  { path: '' , redirectTo: 'cmd', pathMatch:'full'},
-  { path: 'cmd' , component: CommandComponent}
+const routes: Routes = [
+  { path: '' , redirectTo: 'cmd', pathMatch: 'full'},
+  { path: 'cmd' , component: CommandComponent},
+  { path: 'home' , component: HomeComponent}
 ];
 
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports:[RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from'@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CommandComponent } from './command/command.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommandComponent
+    CommandComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
